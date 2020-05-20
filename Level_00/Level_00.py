2d5a12d9acfd9ba8aea0c7580549efe38670ec94
cixgnf <47581181+cixgnf@users.noreply.github.com>
2020-05-20 09:37:00 +0800

    level 00

--- Level_00/Level_00.py
+++ Level_00/Level_00.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+# http://www.pythonchallenge.com/pc/def/0.html
+
+# print(2**38)
+print(pow(2, 38)
+
 # You will get 274877906944. The address of the next level is
-#Http: //www.pythonchallenge.com/pc/def/274877906944.html
+# http: //www.pythonchallenge.com/pc/def/274877906944.html
