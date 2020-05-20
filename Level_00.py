eb8c25509846ef7f4dca147b63cb26e7eb234c23
cixgnf <47581181+cixgnf@users.noreply.github.com>
2020-05-20 09:35:41 +0800

    Update and rename Level_00 to Level_00.py

---
+++ Level_00.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+# http://www.pythonchallenge.com/pc/def/0.html
+
+# print(2**38)
+print(pow(2, 38)
+
+# You will get 274877906944. The address of the next level is
+# http: //www.pythonchallenge.com/pc/def/274877906944.html
